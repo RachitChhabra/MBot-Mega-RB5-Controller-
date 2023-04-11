de5e0d043487fdb772ca4f3900f5b966ef4cf76d
Rachhit Chhabra <61691300+RachitChhabra@users.noreply.github.com>
2023-04-11 12:48:57 -0700

    Delete SLAM directory

--- SLAM/untitled folder/rb5_control_hw3/src/test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-from numpy import genfromtxt
-my_data = genfromtxt("/data/sig.csv", delimiter=',')
-my_data = np.load("/data/sig.npy")
-print(my_data)
-
-# wp1 = np.argmin(data[:,0:2] - [0,0])
